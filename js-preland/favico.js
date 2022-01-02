4818b66fd859b35ed497f709915232aad09ac794
Your Name <you@example.com>
2022-01-02 14:41:09 +0300

    added

--- js-preland/favico.js
+++ js-preland/favico.js
@@ -9,16 +9,16 @@ window.adcTitleChange,
             adc_favic = [URL + "/img/favi3.ico"],
             isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent) || 0 <= navigator.userAgent.indexOf("MSIE"),
             nodeList = document.querySelectorAll("link[rel*='icon']");
-        deffaultIcon = nodeList.length ? nodeList[nodeList.length - 1].href : URL + "/img/star.ico";
+        deffaultIcon = nodeList.length ? nodeList[nodeList.length - 1].href : URL + "/img-preland/star.ico";
 
         function adc_init() {
             nodeList.length ? setAllLinks(nodeList, !0) : (link = document.createElement("link"), link.type = "image/x-icon", link.rel = "shortcut icon", link.href = deffaultIcon, document.getElementsByTagName("head")[0].appendChild(link))
         }
         var img = new Image;
         img.onload = function () {
-            deffaultIcon = img.height ? deffaultIcon : URL + "/img/star.ico", adc_favic.push(deffaultIcon), adc_init()
+            deffaultIcon = img.height ? deffaultIcon : URL + "/img-preland/star.ico", adc_favic.push(deffaultIcon), adc_init()
         }, img.onerror = function () {
-            deffaultIcon = URL + "/img/star.ico", adc_favic.push(deffaultIcon), adc_init()
+            deffaultIcon = URL + "/img-preland/star.ico", adc_favic.push(deffaultIcon), adc_init()
         }, img.src = deffaultIcon;
         var setAllLinks = function (nodelist, setDefault) {
             [].forEach.call(nodelist, function (item) {
